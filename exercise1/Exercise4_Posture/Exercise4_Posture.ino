35d653bf274db94e9d945bbdca43ca4f6abc1591
SeeBehr <sebi.baer@gmx.de>
2024-10-24 10:36:29 +0200

    .

--- exercise1/Exercise4_Posture/Exercise4_Posture.ino
+++ exercise1/Exercise4_Posture/Exercise4_Posture.ino
@@ -7,17 +7,17 @@ unsigned long microsPerReading, microsPrevious;
 float accelScale, gyroScale;
 int counter;
 
-void setup() {
+void setup()
+{
   pinMode(LEDR, OUTPUT);
   pinMode(LEDG, OUTPUT);
-
-  digitalWrite(LEDR, LOW);
-  digitalWrite(LEDG, LOW);
   Serial.begin(1200);
 
-  while(!Serial);
+  while (!Serial)
+    ;
 
-  if (!IMU.begin()) {
+  if (!IMU.begin())
+  {
     Serial.println("Failed to initialize IMU!");
   }
   Serial.print("Accelerometer sample rate = ");
@@ -25,7 +25,7 @@ void setup() {
   Serial.println("Hz");
   Serial.println();
 
-  Serial.print("Gyroscope sample rate = ");  
+  Serial.print("Gyroscope sample rate = ");
   Serial.print(IMU.gyroscopeSampleRate());
   Serial.println("Hz");
   Serial.println();
@@ -35,7 +35,8 @@ void setup() {
   microsPrevious = micros();
 }
 
-void loop() {
+void loop()
+{
   float aix, aiy, aiz;
   float gix, giy, giz;
   float ax, ay, az;
@@ -44,12 +45,11 @@ void loop() {
   unsigned long microsNow;
   int temperature;
   bool alert = false;
-  int ledvalue;
-  int ledvalue2;
 
   // check if it's time to read data and update the filter
   microsNow = micros();
-  if (microsNow - microsPrevious >= microsPerReading) {
+  if (microsNow - microsPrevious >= microsPerReading)
+  {
 
     // read raw data from IMU
     IMU.readAcceleration(aix, aiy, aiz);
@@ -67,16 +67,13 @@ void loop() {
     filter.updateIMU(gx, gy, gz, ax, ay, az);
 
     // Read the temperature from the IMU sensor
-    if (IMU.temperatureAvailable()) {
+    if (IMU.temperatureAvailable())
+    {
       IMU.readTemperature(temperature);
 
-      // Print the temperature to the serial monitor
-      Serial.print("Temperature: ");
-      Serial.print(temperature);
-      Serial.println(" °C");
-
       // Control RGB LED based on the temperature
-      if (!(temperature >= 20 && temperature <= 32)) {
+      if (!(temperature >= 20 && temperature <= 32))
+      {
         alert = true;
       }
     }
@@ -85,50 +82,57 @@ void loop() {
     roll = filter.getRoll();
     pitch = filter.getPitch();
     heading = filter.getYaw();
-    if (counter % 50 == 0) {
-    Serial.print("Orientation: ");
-    Serial.print(heading);
-    Serial.print(" ");
-    Serial.print(pitch);
-    Serial.print(" ");
-    Serial.println(roll);
-    counter = 0;
+    if (counter % 50 == 0)
+    {
+      Serial.print("Orientation: ");
+      Serial.print(heading);
+      Serial.print(" ");
+      Serial.print(pitch);
+      Serial.print(" ");
+      Serial.println(roll);
+      // Print the temperature to the serial monitor
+      Serial.print("Temperature: ");
+      Serial.print(temperature);
+      Serial.println(" °C");
+      counter = 0;
     }
-    if (pitch < -100 && pitch > -80) {
+    if (pitch < -100 || pitch > -80)
+    {
       alert = true;
     }
 
-    if (alert) {
-      ledvalue = 255;
-      ledvalue2 = 0;
-    } else {
-      ledvalue = 0;
-      ledvalue2 = 255;
+    if (alert)
+    {
+      digitalWrite(LEDR, HIGH);
+      digitalWrite(LEDG, LOW);
     }
-
-
-    analogWrite(LEDR, ledvalue);
-    analogWrite(LEDG, ledvalue2);
+    else
+    {
+      digitalWrite(LEDR, LOW);
+      digitalWrite(LEDG, HIGH);
+    }
     // increment previous time, so we keep proper pace
     microsPrevious = microsPrevious + microsPerReading;
     counter++;
   }
 }
 
-float convertRawAcceleration(int aRaw) {
+float convertRawAcceleration(int aRaw)
+{
   // since we are using 2 g range
   // -2 g maps to a raw value of -32768
   // +2 g maps to a raw value of 32767
-  
+
   float a = (aRaw * 2.0) / 32768.0;
   return a;
 }
 
-float convertRawGyro(int gRaw) {
+float convertRawGyro(int gRaw)
+{
   // since we are using 250 degrees/seconds range
   // -250 maps to a raw value of -32768
   // +250 maps to a raw value of 32767
-  
+
   float g = (gRaw * 250.0) / 32768.0;
   return g;
 }
